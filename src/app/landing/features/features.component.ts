02bb854b8a94170f40739d36ded90fea825d5f5f
Alexandr Makarov <speqtr.91@gmail.com>
2022-10-26 20:53:31 +0300

    added scripts burger and counter

--- src/app/landing/features/features.component.ts
+++ src/app/landing/features/features.component.ts
@@ -13,3 +13,43 @@ export class FeaturesComponent implements OnInit {
   }
 
 }
+//счётчик 
+// const counterOne = () => {
+//   let counts;
+//   let upto = 72000000;
+//   let offSetTop = document.querySelector(".features__info-box-count");
+//   let clientHeight = document.body.offsetHeight;
+
+//   function updated() {
+//     let count = document.querySelector(".features__info-box-count");
+//     count.textContent = ++upto + "+";
+//     if (upto === 82000000) {
+//       clearInterval(counts);
+//       count.textContent = "82,000,000+";
+//     }
+//   }
+
+//   if (offSetTop.offsetTop - clientHeight <= window.scrollY) {
+//     counts = setInterval(updated, 1);
+//   }
+// };
+
+// const counterTwo = () => {
+//   let counts;
+//   let upto = 1;
+//   let offSetTop = document.querySelector(".features__info-box-count");
+//   let clientHeight = document.body.offsetHeight;
+
+//   function updated() {
+//     let count = document.querySelector(".features__info-box-count");
+//     count.textContent = ++upto + "M+";
+//     if (upto === 100) {
+//       clearInterval(counts);
+//       count.textContent = "100M+";
+//     }
+//   }
+
+//   if (offSetTop.offsetTop - clientHeight <= window.scrollY) {
+//     counts = setInterval(updated, 1);
+//   }
+// };
