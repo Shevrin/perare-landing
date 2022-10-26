1d497df817fad5fc0b59072dc773b2107d90c995
Pavel Shevrin <vestax747@gmail.com>
2022-10-26 23:46:16 +0300

    call, welcome: add redirect method

--- src/app/landing/call/call.component.ts
+++ src/app/landing/call/call.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-call',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CallComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  public openMap(): void {
+    console.log('redirect to map')
+    // this.router.navigate(['map'])
+  }
 }
